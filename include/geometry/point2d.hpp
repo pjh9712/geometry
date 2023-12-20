80ceecb1c7b0115c66ceab0716674926e546eb5f
pjh9712 <ses88498@gmail.com>
2023-12-20 18:22:51 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author Jeonghoon Park (ses88498@gmail.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023 Programmers, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors: Jeonghoon Park
+
+#ifndef GEOMETRY__POINT_2D_HPP_
+#define GEOMETRY__POINT_2D_HPP_
+
+#endif // GEOMETRY__POINT_2D_HPP_
