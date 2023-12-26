fd5b42e1003c65846263bd7eb5853b6d5175a4e8
pjh9712 <ses88498@gmail.com>
2023-12-26 21:54:30 +0900

    Test(geometry): Add unit test for Point2D assignment operator

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -15,4 +15,10 @@ TEST(GeometryPoint2D, Constructor) {
   Point2D point3(point2);
   Point2D point4(std::move(Point2D()));
 }
+
+TEST(GeometryPoint2D, AssigmentOperator) {
+  Point2D point1;
+  auto point2 = point1;
+  auto point3 = std::move(Point2D());
+}
 }  // namespace geometry
