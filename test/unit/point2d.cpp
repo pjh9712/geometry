ba9e26646ffd8ce881e3a5b2c46ecdb48cca3e0f
pjh9712 <ses88498@gmail.com>
2023-12-26 21:15:25 +0900

    Add point2d test skeleton code

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -1,7 +1,11 @@
 // Copyright (c) 2023 Programmers, All Rights Reserved.
 // Authors: Jeonghoon Park
 
+#include "geometry/point2d.hpp"
+
 #include <limits>
 #include <stdexcept>
+
+#include "gtest/gtest.h"
 
 namespace geometry {} // namespace geometry
