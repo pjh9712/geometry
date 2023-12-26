eca3e1aa3a871e13a1324c1d2aae66b32767edd1
pjh9712 <ses88498@gmail.com>
2023-12-26 22:15:03 +0900

    Feat(geometry): Implement CalculateDistance method in Point2D class

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -4,7 +4,7 @@
  * @brief Point class developments with 2-dimension
  * @version 1.0.0
  * @date 2023-12-22
- * @copyright Copyright (c) 2023 Programmers, All Rights Reserved.
+ * @copyright Copyright (c) 2023 &, All Rights Reserved.
  */
 
 // Copyright (c) 2023 Programmers, All Rights Reserved.
@@ -16,4 +16,9 @@
 
 namespace geometry {
 Point2D::Point2D(double input_x, double input_y) : x_(input_x), y_(input_y) {}
+
+auto Point2D::CalculateDistance(const Point2D& target) const -> double {
+  return std::sqrt(std::pow((x_ - target.x_), 2) +
+                   std::pow((y_ - target.y_), 2));
+}
 }  // namespace geometry
