48a992ef9e172d4af08c4536bcfb0e929b4f6e28
pjh9712 <ses88498@gmail.com>
2023-12-27 19:50:09 +0900

    Feat(geometry): Implement greater than operator for Distance class

--- src/distance.cpp
+++ src/distance.cpp
@@ -87,4 +87,8 @@ auto Distance::operator<=(const Distance& other) const -> bool {
   return (nanometer_ <= other.nanometer_);
 }
 
+auto Distance::operator>(const Distance& other) const -> bool {
+  return (nanometer_ > other.nanometer_);
+}
+
 }  // namespace geometry
