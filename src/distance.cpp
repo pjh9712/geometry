86743376e0320f4808b252150db50a7440c90e05
pjh9712 <ses88498@gmail.com>
2023-12-27 15:40:10 +0900

    Feat(geometry): Implement Distance class constructor with input value

--- src/distance.cpp
+++ src/distance.cpp
@@ -12,4 +12,30 @@
 
 #include "geometry/distance.hpp"
 
-namespace geometry {}  // namespace geometry
+#include <cstdint>
+
+namespace {
+auto ScaleDistanceToNanometer(double input_value,
+                              geometry::Distance::Type input_type)
+    -> int64_t {
+  int64_t result{static_cast<int64_t>(input_value)};
+  if (input_type == geometry::Distance::Type::kKilometer) {
+    result = static_cast<int64_t>(input_value * 1.0e+12);
+  } else if (input_type == geometry::Distance::Type::kMeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+9);
+  } else if (input_type == geometry::Distance::Type::kCentimeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+7);
+  } else if (input_type == geometry::Distance::Type::kMillimeter) {
+    result = static_cast<int64_t>(input_value * 1.0e+6);
+  } else if (input_type == geometry::Distance::Type::kMicrometer) {
+    result = static_cast<int64_t>(input_value * 1.0e+3);
+  }
+  return result;
+}
+}  // namespace
+
+namespace geometry {
+
+Distance::Distance(double input_value, Type input_type)
+    : nanometer_(ScaleDistanceToNanometer(input_value, input_type)) {}
+}  // namespace geometry
